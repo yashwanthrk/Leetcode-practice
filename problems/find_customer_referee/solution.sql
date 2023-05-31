5bf6e953d3e7a67800eec5bc55b97e5f1f492b73
Yashwanth RK <yashwanth1005@gmail.com>
2023-05-31 03:47:20 +0000

    Sync LeetCode submission - Find Customer Referee (mysql)

--- problems/find_customer_referee/solution.sql
+++ problems/find_customer_referee/solution.sql
@@ -1,9 +1,1 @@
-# Write your MySQL query statement below
-select name from customer where  referee_id != 2 or referee_id is NULL ;
-
-
-# SELECT name
-# FROM Customer
-# WHERE COALESCE(referee_id,0) <> 2;
-
-# here COALESCE is used to replace NULL values with zero before checking whether it is equal to 2 or not.
+select name from customer where referee_id != 2 or referee_id is null 
