eb640ace8f9588a48398ac84942de76356cb415c
Yashwanth RK <yashwanth1005@gmail.com>
2023-06-01 13:21:42 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -19,7 +19,9 @@ class Solution:
         dp = [0] * (n + 1)
         dp[1] = 1
         dp[2] = 2
+
         for i in range(3, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
+        
         return dp[n]   
                 
