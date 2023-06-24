9208840030d5c8e4f5066a773eeaff6c02fb378e
Yashwanth RK <yashwanth1005@gmail.com>
2023-06-24 17:26:26 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- problems/top_k_frequent_elements/solution.py
+++ problems/top_k_frequent_elements/solution.py
@@ -1,15 +1,28 @@
 from collections import Counter
 
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_counter = Counter(nums)
+        
+        frequencies = {}  
+
+        for element in nums:
+            frequencies[element] = frequencies.get(element, 0) + 1
+
+        # Sort the elements based on their frequencies in descending order
+        sorted_elements = sorted(frequencies, key=frequencies.get, reverse=True)
+        
+        # Select the top k frequent elements
+        top_k_elements = sorted_elements[:k]
+
+        return top_k_elements
+       
+
+
+        
         
 
-        # Sorted parameters are as follows sorted(iterable, key, reverse=?) where iterable is what you wanna iterate through, key is the function you wanna apply, and reverse=True sorts in decreasing order. 
-        # So in this case the iterable will be d.items() which is in the tuple format, key is a lambda fucntion lambda x:x[1], which is telling the sort to happen in terms of the values and reverse=True to sort in a decending order.
-        sorted_nums = sorted(num_counter.items(), key=lambda item: item[1], reverse = True)
-        return [sorted_nums[i][0] for i in range(k)]
-		
-       
-      
+
+                
+                
                 
