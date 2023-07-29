d980aced0eafc2ab66bf754884bacf246f5db280
Yashwanth RK <yashwanth1005@gmail.com>
2023-07-29 17:27:46 +0000

    Sync LeetCode submission - Sort an Array (python3)

---
+++ problems/sort_an_array/solution.py
@@ -0,0 +1,47 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge_sort(nums):
+            
+            if len(nums) <= 1:
+                return
+
+            mid = len(nums) // 2
+            left = nums[:mid]
+            right = nums[mid:]
+
+            # Recursive call on each half
+            merge_sort(left)
+            merge_sort(right)
+
+            # Two iterators for traversing the two halves
+            i = 0
+            j = 0
+            
+            # Iterator for the main list
+            k = 0
+            
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                # The value from the left half has been used
+                    nums[k] = left[i]
+                    # Move the iterator forward
+                    i += 1
+                else:
+                    nums[k] = right[j]
+                    j += 1
+                # Move to the next slot
+                k += 1
+
+            # For all the remaining values
+            while i < len(left):
+                nums[k] = left[i]
+                i += 1
+                k += 1
+
+            while j < len(right):
+                nums[k]=right[j]
+                j += 1
+                k += 1
+
+        merge_sort(nums)
+        return nums
