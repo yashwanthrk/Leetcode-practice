918c6082893ea4e12e12192db1ac4e6a49b9c7a3
Yashwanth RK <yashwanth1005@gmail.com>
2023-08-08 23:23:25 +0000

    Sync LeetCode submission - Linked List Cycle II (python3)

---
+++ problems/linked_list_cycle_ii/solution.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not (head and head.next):
+            return
+            
+        current = head
+        seen = set()
+        while (current != None):
+            if current in seen:
+                return current
+            else:
+                seen.add(current)
+            current = current.next
+        return None
