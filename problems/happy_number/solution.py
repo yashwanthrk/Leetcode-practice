f010b05937432fc400fd3c0dc6fe6a4e653d2c26
Yashwanth RK <yashwanth1005@gmail.com>
2023-08-08 22:02:21 +0000

    Sync LeetCode submission - Happy Number (python3)

--- problems/happy_number/solution.py
+++ problems/happy_number/solution.py
@@ -2,24 +2,39 @@ class Solution:
     
     def isHappy(self, n: int) -> bool:
         
-        def check_happy_number(n):
-            print(n)
-            if n > 1 and n < 7 or n in [8, 9]:
-                return False
+        # def check_happy_number(n):
+        #     print(n)
+        #     if n > 1 and n < 7 or n in [8, 9]:
+        #         return False
 
-            if n == 1 or n == 7:
-                return True
+        #     if n == 1 or n == 7:
+        #         return True
 
-            # n = str(n)
-            # sum = 0
-            # for x in n:
-            #     sum += (int(x) ** 2)
-            n = str(n)
-            sum_val = sum(int(x) ** 2 for x in n)
+        #     # n = str(n)
+        #     # sum = 0
+        #     # for x in n:
+        #     #     sum += (int(x) ** 2)
+        #     n = str(n)
+        #     sum_val = sum(int(x) ** 2 for x in n)
            
-            return check_happy_number(sum_val)
+        #     return check_happy_number(sum_val)
 
-        return check_happy_number(n)
+        # return check_happy_number(n)
+        
+        # use set to store the numbers that was found in the iteration
+        # inc = set()
+
+        # while (n not in inc) and n != 1:
+        #     inc.add(n)
+        #     res = 0 # number to store temporary sum
+        #     while n:
+        #         res += (n % 10) ** 2
+        #         n = (n // 10)
+        #     n = res
+        
+        # return True if n == 1 else False
+
+
 
         # def recursive_call(n, loop_dict):
             
@@ -44,3 +59,18 @@ class Solution:
         #         # check if its the infinite loop
         #         return recursive_call(sum, loop_dict)
         # return recursive_call(n, {})
+
+
+        def get_next(num):
+            return sum(int(digit) ** 2 for digit in str(num))
+
+        # Create a slow pointer (tortoise) and a fast pointer (hare).
+        
+        tortoise = n
+        hare = get_next(n)
+        
+        while hare != 1 and hare != tortoise:
+            tortoise = get_next(tortoise)
+            hare = get_next(get_next(hare))  # Move hare two steps.
+            
+        return hare == 1
