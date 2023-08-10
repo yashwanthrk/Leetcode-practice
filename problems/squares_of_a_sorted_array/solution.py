76cfe8f2ecd1100ea070a6fe76dea95a0bc2b69f
Yashwanth RK <yashwanth1005@gmail.com>
2023-08-10 00:59:39 +0000

    Sync LeetCode submission - Squares of a Sorted Array (python3)

--- problems/squares_of_a_sorted_array/solution.py
+++ problems/squares_of_a_sorted_array/solution.py
@@ -1,26 +1,26 @@
 class Solution:
       def sortedSquares(self, nums: List[int]) -> List[int]:
-        res = []  
-        left, right = 0, len(nums)-1  
-        
+
+        n = len(nums)
+        result_array = [0] * n
+        left, right = 0, n - 1
+
+        # very impo
+        pos = n - 1
+
+
         while left <= right:
-            if nums[left] < 0:  
-                # if the element pointed by left pointer is negative
-                
-                if abs(nums[left]) >= nums[right]:
-                    res.append(nums[left]**2)  
-                    left += 1  
-                else:
-                    res.append(nums[right]**2)  
-                    right -= 1  
-            else:  
-                break
-        
-        # loop through the remaining elements from right pointer to left pointer and 
-        # append their squares to the result array
-        for i in range(right, left-1, -1):
-            res.append(nums[i]**2)
-        
-         # reverse the result array to get the squares in non-decreasing order
-        res.reverse() 
-        return res
+
+          left_sqr = nums[left] ** 2
+          right_sqr = nums[right] ** 2
+
+          if left_sqr > right_sqr:
+            result_array[pos] = left_sqr
+            left += 1
+          else:
+            result_array[pos] = right_sqr
+            right -= 1
+          
+          pos -= 1
+
+        return result_array
