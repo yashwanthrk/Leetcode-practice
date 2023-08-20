53a6e4b155ca3a46dbdf562e4328b1f26905ae04
Yashwanth RK <yashwanth1005@gmail.com>
2023-08-20 19:43:13 +0000

    Sync LeetCode submission - Remove Element (python3)

---
+++ problems/remove_element/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+    
+        k = 0  # Initialize a variable to count elements not equal to val
+        
+        for num in nums:
+            if num != val:
+                nums[k] = num
+                k += 1
+        
+        return k
