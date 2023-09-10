55fbae1f0548500b536e56a33e79bca914ed9f3c
Yashwanth RK <yashwanth1005@gmail.com>
2023-09-10 17:19:32 +0000

    Sync LeetCode submission - Simplify Path (python3)

---
+++ problems/simplify_path/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        components = path.split("/")
+        stack = []
+
+        for component in components:
+            if component == "..":
+                if stack:
+                    stack.pop()
+            elif component and component != ".":
+                stack.append(component)
+        
+        print(stack)
+
+        
+
+        return "/" + "/".join(stack)
