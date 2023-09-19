72d0c7ed5466afbe9a1568071880839f8fc23a1e
Yashwanth RK <yashwanth1005@gmail.com>
2023-09-19 23:42:55 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,35 +1,43 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
-    
-
-        # [58, 59, 77,  2, 82, 83, 50, 89, 34, 99, 24, 30, 44, 43, 58, 18, 88, 48, 15, 33]
-
-
-        if not nums: 
-            return 0
         
-        if len(nums) == 1: 
-            return nums[0]
+
+        # if not nums: 
+        #     return 0
         
-        dp = [0] * len(nums)
+        # if len(nums) == 1: 
+        #     return nums[0]
+        
+        # dp = [0] * len(nums)
 
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[0], nums[1])
 
-        for i in range(2, len(nums)):
-            # print(nums[i], dp[i-2], dp[i-1])
+        # for i in range(2, len(nums)):
+        #     # print(nums[i], dp[i-2], dp[i-1])
 
-            # no adjacent houses; 
-            # either max of not adjacent, or take big number
+        #     # no adjacent houses; 
+        #     # either max of not adjacent, or take big number
             
-            # nums[k] = max(k + nums[k-2], nums[k-1])
+        #     # nums[k] = max(k + nums[k-2], nums[k-1])
 
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
-            # print(dp[i])
+        #     dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        #     # print(dp[i])
 
-        return dp[-1] 
-     
+        # return dp[-1] 
 
-        
 
+        if not nums:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        # set prev1 to 0 as iteration starts from 2 nd element
+        prev1, prev2 = 0, nums[0]
+
+        for i in range(1, len(nums)):
+            current = max(prev2, prev1 + nums[i])
+            prev1, prev2 = prev2, current
+
+        return prev2
