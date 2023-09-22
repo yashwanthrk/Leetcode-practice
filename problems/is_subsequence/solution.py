66f0e29c19859d274b998f1e9db9152a6df411a7
Yashwanth RK <yashwanth1005@gmail.com>
2023-09-22 23:34:39 +0000

    Sync LeetCode submission - Is Subsequence (python3)

--- problems/is_subsequence/solution.py
+++ problems/is_subsequence/solution.py
@@ -1,16 +1,31 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+         
 
-        # copied leetcode discussions 
-        LEFT_BOUND, RIGHT_BOUND = len(s), len(t)
 
-        i = 0
-        j = 0
+        if not s:
+            return True
+                
+        s_i = 0
+        for t_i in t:
+            if s[s_i] == t_i:
+                s_i += 1
+            if s_i == len(s):
+                return True
+        
+        return s_i == len(s)
 
-        while i < LEFT_BOUND and j < RIGHT_BOUND:
-            # move both pointers or just the right pointer
-            if s[i] == t[j]:
-                i += 1
-            j += 1
 
-        return i == LEFT_BOUND
+        # # copied leetcode discussions 
+        # LEFT_BOUND, RIGHT_BOUND = len(s), len(t)
+
+        # i = 0
+        # j = 0
+
+        # while i < LEFT_BOUND and j < RIGHT_BOUND:
+        #     # move both pointers or just the right pointer
+        #     if s[i] == t[j]:
+        #         i += 1
+        #     j += 1
+
+        # return i == LEFT_BOUND
