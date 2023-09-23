2b7f078d113215b2d8f9435483df8b8bb7581346
Yashwanth RK <yashwanth1005@gmail.com>
2023-09-23 18:21:06 +0000

    Sync LeetCode submission - Substrings of Size Three with Distinct Characters (python3)

--- problems/substrings_of_size_three_with_distinct_characters/solution.py
+++ problems/substrings_of_size_three_with_distinct_characters/solution.py
@@ -1,12 +1,24 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
+        
+        # if len(s) <= 2:
+        #     return 0
+
+        # count = 0
+        # for i in range(len(s) - 2):
+        #     cur_sub = s[i:i+3]
+        #     if len(set(cur_sub)) == 3:
+        #         count += 1
+
+        # return count 
+
 
         count = 0
-
-        for index in range(len(s) - 2):
-            if s[index] in s[index+1:index+3] or s[index+1] == s[index+2]:
-                continue
-            else:
+        for i in range(len(s) - 2):
+            if s[i] != s[i + 1] and s[i] != s[i + 2] and s[i + 1] != s[i + 2]:
                 count += 1
-
         return count
+
+            
+
+
