fbec3249516134f4dd9a3cfbbf123d1623379613
Yashwanth RK <yashwanth1005@gmail.com>
2023-10-10 00:23:56 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,17 +1,48 @@
 class Solution:
     def searchRange(self, nums, target):
-        def binarySearch(nums, target, first):
-            left, right = 0, len(nums)
-            while left < right:
+        
+        # https://www.youtube.com/watch?v=6zhGS79oQ4k&ab_channel=takeUforward
+
+        # Striver videos
+
+        def lower_bound(nums, target):
+            left, right = 0, len(nums) - 1
+            ans = -1
+            while left <= right:
                 mid = (left + right) // 2
-                if nums[mid] > target or (first and target == nums[mid]):
-                    right = mid
+
+                # only difference
+                if nums[mid] >= target: 
+                    ans = mid
+                    right = mid - 1
                 else:
                     left = mid + 1
-            return left
+            return ans
 
-        first = binarySearch(nums, target, True)
-        if first == len(nums) or nums[first] != target:
+        
+        def upper_bound(nums, target):
+                left, right = 0, len(nums) - 1
+                ans = -1
+                while left <= right:
+                    mid = (left + right) // 2
+
+                    # only difference
+                    if nums[mid] > target: 
+                        ans = mid - 1
+                        right = mid - 1
+                    else:
+                        if nums[mid] == target:  # Update answer if the element is found
+                            ans = mid
+                        left = mid + 1
+
+                return ans
+        
+        left = lower_bound(nums, target)
+        right = upper_bound(nums, target)
+
+        if left <= right:
+            return [left, right]
+        else:
             return [-1, -1]
-        last = binarySearch(nums, target, False)
-        return [first, last-1]
+
+
