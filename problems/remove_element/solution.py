5a578bc7d21c7bee17b86ae87bcde978a90246b9
Yashwanth RK <yashwanth1005@gmail.com>
2023-11-17 13:14:35 +0000

    Sync LeetCode submission - Remove Element (python3)

--- problems/remove_element/solution.py
+++ problems/remove_element/solution.py
@@ -3,9 +3,9 @@ class Solution:
     
         k = 0  # Initialize a variable to count elements not equal to val
         
-        for num in nums:
-            if num != val:
-                nums[k] = num
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
                 k += 1
-        
+
         return k
