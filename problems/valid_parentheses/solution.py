eeff5f3620baf9c3bcd6686402b6e8241adfe434
Yashwanth RK <yashwanth1005@gmail.com>
2023-12-23 05:07:46 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- problems/valid_parentheses/solution.py
+++ problems/valid_parentheses/solution.py
@@ -2,17 +2,20 @@ class Solution:
     def isValid(self, s: str) -> bool:
         
         stack = []
-        parentheses_dict = { "}" : "{", ")" : "(", "]" : "["}
 
-        for c in s:
-            if c not in parentheses_dict:
-                stack.append(c)
-            else:
-                if stack and stack[-1] == parentheses_dict[c]:
-                    stack.pop()
-                else:
-                    return False
+        parenthesis_dict = { ')' : '(' ,  "}" :  "{" ,    "]" : "[" }
+
+        for ch in s:
+            if ch not in parenthesis_dict:
+                stack.append(ch)
                 
+            elif stack and stack[-1] == parenthesis_dict[ch]:
+                stack.pop()
+            
+            else:
+                stack.append(ch)
+
+            
 
         return len(stack) == 0
 
