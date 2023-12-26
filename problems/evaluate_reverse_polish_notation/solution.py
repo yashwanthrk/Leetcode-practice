0c1b9e34fe79b36d7367c59734f3cd0ba0482dfc
Yashwanth RK <yashwanth1005@gmail.com>
2023-12-26 02:40:05 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (python)

--- problems/evaluate_reverse_polish_notation/solution.py
+++ problems/evaluate_reverse_polish_notation/solution.py
@@ -1,58 +1,37 @@
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-    #     dummy_stack = []
-    #     for token in tokens:
-    #         try:
-    #             token = int(token)
-    #             dummy_stack.append(int(token))
-    #         except:
-    #             num2 = dummy_stack.pop()
-    #             num1 = dummy_stack.pop()
-    #             if token == "+":
-    #                 result = self.rpn_add(num1, num2)
-    #             elif token == "-":
-    #                 result = self.rpn_sub(num1, num2)
-    #             elif token == "*":
-    #                 result = self.rpn_multiply(num1, num2)
-    #             else:
-    #                 result = int(self.rpn_divide(num1, num2))
-                
-    #             dummy_stack.append(result)
-
-    #     return dummy_stack[0]
-                
-
-
-    # def rpn_add(self, num1, num2):
-    #     return num1 + num2
-    
-    # def rpn_sub(self, num1, num2):
-    #     return num1 - num2
-        
-    # def rpn_multiply(self, num1, num2):
-    #     return num1 * num2
-    
-    # def rpn_divide(self, num1, num2):
-    #     return int(float(num1) / (num2))
-
-
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
 
 
         stack = []
+
+        operators = ["+", "-", "*", "/"]
+
         for token in tokens:
-            if token in {'+', '-', '*', '/'}:
-                b = stack.pop()
-                a = stack.pop()
-                if token == '+':
-                    stack.append(a+b)
+            
+            if token in operators and len(stack) > 1:
+                
+                second_num = stack.pop()
+                first_num = stack.pop()
+
+                result = 0
+                
+                if token == "+":
+                    result = first_num + second_num
                 elif token == '-':
-                    stack.append(a-b)
-                elif token == '*':
-                    stack.append(a*b)
+                    result = first_num - second_num
+                elif token == "*":
+                    result = first_num * second_num
                 else:
-                    stack.append(int(a/b))
+                    result = int(float(first_num) / second_num)
+                
+                stack.append(result)
+
             else:
                 stack.append(int(token))
         
-        return stack[-1]
 
+        return stack.pop()
