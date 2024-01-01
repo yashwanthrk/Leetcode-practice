a266234cc2404118d476feca26233b44219b73ec
Yashwanth RK <yashwanth1005@gmail.com>
2024-01-01 19:21:31 +0000

    Sync LeetCode submission - Assign Cookies (python)

---
+++ problems/assign_cookies/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        
+        g.sort()
+        s.sort()
+
+        i = j = 0
+        while i < len(g):
+            while j < len(s) and g[i] > s[j]:
+                j += 1
+            
+            if j == len(s):
+                break
+            
+            i += 1
+            j += 1
+        
+        return i
