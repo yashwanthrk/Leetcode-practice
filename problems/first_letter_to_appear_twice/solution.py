b77dede660fae051c310d11fb547f549de6ac8d9
Yashwanth RK <yashwanth1005@gmail.com>
2024-02-05 03:18:25 +0000

    Sync LeetCode submission - First Letter to Appear Twice (python3)

---
+++ problems/first_letter_to_appear_twice/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+
+        seen = set()
+
+        for ch in s:
+            if ch in seen:
+                return ch
+            
+            seen.add(ch)
