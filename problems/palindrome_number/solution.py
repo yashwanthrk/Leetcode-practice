9e4e57fe094ba9d5df24461b65e2fd6cb84aefec
Yashwanth RK <yashwanth1005@gmail.com>
2024-02-09 18:45:02 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- problems/palindrome_number/solution.py
+++ problems/palindrome_number/solution.py
@@ -1,13 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
 
-        x = str(x)
-        i = 0
-        j = len(x) - 1
-        while i <= j:
-            if x[i] != x[j]:
-                return False
-            else:
-                i += 1
-                j -= 1
-        return True
+# If x ends with a 0 (x % 10 == 0) and x is not 0, 
+# it cannot be a palindrome. 
+# For example, 10, 20, 30, etc., are not palindromes since there's no leading zero to match the trailing zero.
+        
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        reversed_num = 0
+        original = x
+
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+
+        return x == reversed_num or x == reversed_num // 10
