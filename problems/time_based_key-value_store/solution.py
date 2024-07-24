48275d4b50248d9dffb57065a5266d0f8215b78c
Yashwanth RK <yashwanth1005@gmail.com>
2024-07-24 02:11:58 +0000

    Sync LeetCode submission - Time Based Key-Value Store (python3)

--- problems/time_based_key-value_store/solution.py
+++ problems/time_based_key-value_store/solution.py
@@ -1,38 +1,23 @@
+from collections import defaultdict
+
 class TimeMap:
 
     def __init__(self):
-        # key : list of [value, timestamp]
-        self.key_time_map = {} 
-        
+        self.store = defaultdict(list)
+
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.key_time_map:
-            self.key_time_map[key] = []
-        
-        self.key_time_map[key].append([value, timestamp])
+        self.store[key].append((timestamp, value))
         
 
     def get(self, key: str, timestamp: int) -> str:
-        values = self.key_time_map.get(key, [])
+        values = self.store.get(key, [])
 
-        res = ""
-        l, r = 0, len(values) - 1
-        while l <= r:
-            m = (l + r) // 2
-            if values[m][1] <= timestamp:
+        for time, val in reversed(values):
+            if time <= timestamp:
+                return val
 
-                # Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns ""
-                res = values[m][0]
-                l = m + 1
-            else:
-                r = m - 1
-        
-        return res
-
-
-
-       
-
+        return ""
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
