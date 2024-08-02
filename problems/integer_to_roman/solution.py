fa8f82a4c432ccea577c7df5ffdfa17a2520e06c
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-02 23:46:14 +0000

    Sync LeetCode submission - Integer to Roman (python3)

---
+++ problems/integer_to_roman/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbols = {
+                "M":	1000,
+                "CM": 900,
+                "D":	500,
+                "CD": 400,
+                "C":	100,
+                "XC": 90,
+                "L":	50,
+                "XL": 40,
+                "X":	10,
+                "IX": 9,
+                "V":	5,
+                "IV": 4,
+                "I":	1,
+        }
+
+
+        roman_symbol = ""
+
+        for sym, val in  symbols.items():
+            while num >= val:
+                roman_symbol += sym
+                num -= val
+        
+        return roman_symbol
