67e5a0d29e95c298e2e47d7e90022e0c140e4182
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-04 23:13:42 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,43 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-
-        # if not nums: 
-        #     return 0
-        
-        # if len(nums) == 1: 
-        #     return nums[0]
-        
-        # dp = [0] * len(nums)
-
-        # dp[0] = nums[0]
-        # dp[1] = max(nums[0], nums[1])
-
-        # for i in range(2, len(nums)):
-        #     # print(nums[i], dp[i-2], dp[i-1])
-
-        #     # no adjacent houses; 
-        #     # either max of not adjacent, or take big number
-            
-        #     # nums[k] = max(k + nums[k-2], nums[k-1])
-
-        #     dp[i] = max(nums[i] + dp[i-2], dp[i-1])
-        #     # print(dp[i])
-
-        # return dp[-1] 
-
-
-        if not nums:
+        if not nums: 
             return 0
-
-        if len(nums) == 1:
+        
+        if len(nums) == 1: 
             return nums[0]
+        
+        dp = [0] * len(nums)
 
-        # set prev1 to 0 as iteration starts from 2 nd element
-        prev1, prev2 = 0, nums[0]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
-        for i in range(1, len(nums)):
-            current = max(prev2, prev1 + nums[i])
-            prev1, prev2 = prev2, current
+        for i in range(2, len(nums)):
 
-        return prev2
+            # no adjacent houses; 
+            # either max of not adjacent, or take big number            
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+
+        return dp[-1] 
