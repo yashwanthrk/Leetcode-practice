03f508f8b2f641f2fb4218eadbbf31edf2b22ba1
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-05 02:16:51 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
        
-        if not n:
+        if n == 0:
             return 0
+        
         if n == 1:
             return 1
+        
+        if n == 2:
+            return 2
 
         prev1, prev2 = 1, 2
-
         for _ in range(3, n + 1):
             current = prev1 + prev2
             prev1, prev2 = prev2, current
