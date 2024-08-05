44efd2799d2fd3ec4c8015519b7ca278c5b71243
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-05 00:09:35 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -1,49 +1,24 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-
-        root = ListNode()
-        curr = root
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
 
         while list1 and list2:
-            
             if list1.val <= list2.val:
-                curr.next = ListNode(list1.val)
-                curr = curr.next
-                if list1:
-                    list1 = list1.next
+                curr.next = list1
+                list1 = list1.next
+
             else:
-                curr.next = ListNode(list2.val)
-                curr = curr.next
-                if list2:
-                    list2 = list2.next
-           
-                
-        
-        while list1:
-            curr.next = ListNode(list1.val)
-            list1 = list1.next
+                curr.next = list2
+                list2 = list2.next
+
             curr = curr.next
 
+        curr.next = list1 if list1 else list2
 
-        while list2:
-            curr.next = ListNode(list2.val)
-            list2 = list2.next
-            curr = curr.next
-
-        
-        return root.next
-
-                    
-
-
-        
+        return dummy.next
