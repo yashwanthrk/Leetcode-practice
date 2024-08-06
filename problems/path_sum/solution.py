aa50ce89610b8ceb4776ca8b50a08753776be2da
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-06 02:10:36 +0000

    Sync LeetCode submission - Path Sum (python3)

--- problems/path_sum/solution.py
+++ problems/path_sum/solution.py
@@ -1,33 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def hasPathSum(self, root, targetSum):
-        """
-        :type root: TreeNode
-        :type targetSum: int
-        :rtype: bool
-        """
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         
-        def calculate_sum(node, current_sum):
-            
-            if not node:
-                return False
+        if not root:
+            return False
 
-            current_sum += node.val
+        if not root.left and not root.right:
+            return root.val == targetSum
 
-            # Check if it's a leaf node and if the sum matches the targetSum
-            if not node.left and not node.right:
-                return current_sum == targetSum
+        targetSum -= root.val
 
-            # Recursively check the left and right subtrees
-            left_path = calculate_sum(node.left, current_sum)
-            print(current_sum)
-            right_path = calculate_sum(node.right, current_sum)
-
-            return left_path or right_path
-
-        return calculate_sum(root, 0)
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
