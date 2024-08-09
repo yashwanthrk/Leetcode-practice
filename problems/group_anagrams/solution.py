22c6ec3c88fb422fb5e3dbaed94e47cb48616b34
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-09 00:51:56 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- problems/group_anagrams/solution.py
+++ problems/group_anagrams/solution.py
@@ -1,12 +1,23 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        anagram_groups = defaultdict(list)
-    
-        for string_str in strs:
-            sorted_string = ''.join(sorted(string_str))
-            
-            anagram_groups[sorted_string].append(string_str)
+        result = {}
 
-      
-        return list(anagram_groups.values())
+        for s in strs:
+            count = [0] * 26
+            for char in s:
+                # ord(char): This part of the expression converts a character (e.g., 'a', 'b', etc.) into its corresponding ASCII value. 
+                # For instance, the ASCII value of 'a' is 97.
+# ord('a'): This is a constant value, 97, which is the ASCII value of the letter 'a'.
+                count[ord(char) - ord('a')] += 1
+            
+            # Convert the count list to a tuple to use as a dictionary key
+            key = tuple(count)
+
+            
+            if key not in result:
+                result[key] = []
+
+            result[key].append(s)
+        
+        return list(result.values())
