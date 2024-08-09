bad1f58c2d8f0d46393b54b80163c9c2e7859318
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-09 06:46:24 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- problems/top_k_frequent_elements/solution.py
+++ problems/top_k_frequent_elements/solution.py
@@ -1,28 +1,22 @@
-from collections import Counter
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        frequencies = {}  
-
-        for element in nums:
-            frequencies[element] = frequencies.get(element, 0) + 1
-
-        # Sort the elements based on their frequencies in descending order
-        sorted_elements = sorted(frequencies, key=frequencies.get, reverse=True)
+        # Count frequencies of each element
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
         
-        # Select the top k frequent elements
-        top_k_elements = sorted_elements[:k]
-
-        return top_k_elements
-       
-
-
+        # Bucket sort based on frequency
+        freq = [[] for _ in range(len(nums) + 1)]
+        for num, frequency in count.items():
+            freq[frequency].append(num)
         
-        
-
-
-                
-                
-                
+        # Gather top k frequent elements
+        result = []
+        for i in range(len(freq) - 1, 0, -1):
+            # for i in range(len(freq) - 1, 0, -1): The loop starts from the highest possible frequency (len(nums) since freq has len(nums) + 1 elements) and decrements down to 1. 
+            # This order ensures that the elements with the highest frequencies are considered first. 
+            for num in freq[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
