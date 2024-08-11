5e5366953cf32eea997f63fb5cdd4df4eebd2a74
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-11 16:39:27 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,29 +1,29 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #  striver video
         
-        
-        # Get the minimum integer value
-        max_value = -sys.maxsize - 1
-        current_sum = 0
+        # # Get the minimum integer value
+        # max_value = -sys.maxsize - 1
+        # current_sum = 0
 
-        for num in nums:
-            current_sum += num
+        # for num in nums:
+        #     current_sum += num
             
-            # discard if it reaches negative, we dont need negative sub array
-            if current_sum < 0:
-                current_sum = 0
+        #     # discard if it reaches negative, we dont need negative sub array
+        #     if current_sum < 0:
+        #         current_sum = 0
 
-            max_value = max(max_value, current_sum)
+        #     max_value = max(max_value, current_sum)
 
     
-        return max_value if max_value > 0 else max(nums)
+        # return max_value if max_value > 0 else max(nums)
 
 
-        # dp = [0] * len(nums)
-        # dp[0] = nums[0]
 
-        # for i in range(1, len(nums)):
-        #     dp[i]  = max(dp[i-1] + nums[i], nums[i])
-        #     # print(dp)
-        # return max(dp)
+        max_value = nums[0]
+        current_sum = nums[0]
+
+        for num in nums[1:]:
+            current_sum = max(num, current_sum + num)
+            max_value = max(max_value, current_sum)
+
+        return max_value
