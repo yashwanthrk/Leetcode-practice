3e0f951c1567359917848afa6171db911badda70
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-13 07:45:17 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -2,24 +2,27 @@ class Solution:
     def decodeString(self, s: str) -> str:
         
         stack = []
+
         for char in s:
+
             if char != ']':
                 stack.append(char)
-            else:
-                substr = ""
-                while stack[-1] != '[':
-                    # only alphabets are concatenated now
-                    # order of concatenation is important
-                    substr = stack.pop() + substr
-                # pop last element which is '['
-                stack.pop()
 
+            else:
+                
+                substr = ""
+                while stack and stack[-1] != '[':
+                    substr = stack.pop() + substr
+                
+                # Remove the '[' character
+                stack.pop()
+                
+                # Build the repeat count
                 k = ""
-                # pop digits which is 0 - 9
                 while stack and stack[-1].isdigit():
                     k = stack.pop() + k
-
+                
+                # Repeat the substring k times and push back to the stack
                 stack.append(int(k) * substr)
-
-
+        
         return "".join(stack)
