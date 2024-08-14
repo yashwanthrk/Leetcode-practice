3f55e50182e008a1898c63c85ac1d03588c439d3
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-14 02:54:43 +0000

    Sync LeetCode submission - Happy Number (python3)

--- problems/happy_number/solution.py
+++ problems/happy_number/solution.py
@@ -2,33 +2,24 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         
 
-        seen = set()
+        cycle_set = set()
 
-        def happy_loop(n):
+        def check_for_happy_number(n):
 
-            n = str(n)
-            total = sum([int(ch) * int(ch) for ch in n])
-
-            if total == 1:
-                return True
-
-            elif total in seen:
+            if n in cycle_set:
                 return False
-
-            else:
-                seen.add(total) 
-                return happy_loop(total)
-        
-        return happy_loop(n)
-
-
-
-        # seen = set()
-
-        # while n != 1 and n not in seen:
-        #     seen.add(n)
-        #     n = sum(int(ch) ** 2 for ch in str(n))
-
-        # return n == 1
-
             
+            cycle_set.add(n)
+
+            if n == 1:
+                return True
+            
+            num_ = 0
+            for ch in str(n):
+                num_ += int(ch) * int(ch)
+
+            print(num_)
+
+            return check_for_happy_number(num_)
+
+        return check_for_happy_number(n)
