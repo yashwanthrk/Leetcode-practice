e768f06e053c417c1924c7cbfc42a6d58e91288b
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-17 17:16:10 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- problems/product_of_array_except_self/solution.py
+++ problems/product_of_array_except_self/solution.py
@@ -1,24 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-       
         length = len(nums)
-        
-        # Arrays to store left and right products
-        L, R, answer = [0]*length, [0]*length, [0]*length
 
-        # L[i] contains the product of all the elements to the left of i
-        L[0] = 1
-        for i in range(1, length):
-            L[i] = nums[i - 1] * L[i - 1]
+        left = [1] * len(nums)
+        right = [1] * len(nums)
+        result = [1] * len(nums)
+      
+        for index in range(1, length):
+            left[index] = nums[index - 1] * left[index -1]
 
-        # R[i] contains the product of all the elements to the right of i
-        R[length - 1] = 1
-        for i in range(length - 2, -1, -1):
-            R[i] = nums[i + 1] * R[i + 1]
 
-        # Constructing the answer array
-        for i in range(length):
-            answer[i] = L[i] * R[i]
+        for index in range(len(nums) - 2, -1, -1):
+            right[index] = nums[index + 1] * right[index + 1]
 
-        return answer
+        for index in range(length):
+            result[index] = left[index] * right[index]
+
+        return result
