a00a288e882650c58a35a9e4529ea05b39d4a286
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-18 16:23:52 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,10 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+     
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if target == nums[i] + nums[j]:
+        #             return [i, j]
+
+
+        # return [-1, -1]
+
         num_dict = {}
-        for index, num in enumerate(nums):
-            if num in num_dict:
-                return [num_dict[num], index]
+        for i, num in enumerate(nums):
+            if target - num in num_dict:
+                return [i, num_dict[target - num]]
             else:
-                num_dict[target - num] = index
-        
+                num_dict[num] = i
+
+        return [-1, -1]
+
+           
+
+
