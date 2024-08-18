ecc21026229911599b5ddb0eb45c96d92b246e07
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-18 11:30:29 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- problems/longest_palindromic_substring/solution.py
+++ problems/longest_palindromic_substring/solution.py
@@ -1,36 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-
-
+        
         result_len = 0
         start, end = 0, 0
 
+        def expand_around_center(l, r, start, end, result_len):
+            
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > result_len:
+                    start = l
+                    end = r
+                    result_len = r - l + 1
+                l -= 1
+                r += 1
+            return start, end, result_len
+
         for i in range(len(s)):
-
-            #  odd string
-            l , r = i, i
-            # while pointer is still in bound and palindrome
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if ( (r - l) + 1 ) > result_len:
-                    start = l
-                    end = r
-                    result_len = max((r-l)  + 1, result_len)
-                l -= 1
-                r += 1
-
-            l , r = i, i + 1
-            #  even string
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if ( (r - l) + 1 ) > result_len:
-                    start = l
-                    end = r
-                    result_len = max((r-l)  + 1, result_len)
-                l -= 1
-                r += 1
+            # Odd-length palindrome
+            start, end, result_len = expand_around_center(i, i, start, end, result_len)
+            # Even-length palindrome
+            start, end, result_len = expand_around_center(i, i + 1, start, end, result_len)
 
         return s[start: end + 1]
-
-
-
-            
-
