966a82a4e8639ebc05adca1a14939133649ff6f8
Yashwanth RK <yashwanth1005@gmail.com>
2024-08-25 03:08:07 +0000

    Sync LeetCode submission - Final Array State After K Multiplication Operations I (python3)

---
+++ problems/final_array_state_after_k_multiplication_operations_i/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        
+        for _ in range(k):
+            min_index = nums.index(min(nums))
+            nums[min_index] = nums[min_index] * multiplier
+            
+        return nums
+
