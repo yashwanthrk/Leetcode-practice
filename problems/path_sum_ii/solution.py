fbca307df700cb3924625012c092a99faa9ced07
Yashwanth RK <yashwanth1005@gmail.com>
2024-10-12 19:26:47 +0000

    Sync LeetCode submission - Path Sum II (python3)

--- problems/path_sum_ii/solution.py
+++ problems/path_sum_ii/solution.py
@@ -6,24 +6,35 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        total_paths = []
 
-        def dfs(node, current_sum, path):
-            if not node:
+        
+        result = []
+
+        if root is None:
+            return []
+
+        def dfs(node, remaining_sum, current_path):
+            
+            if node is None:
                 return
             
-            current_sum += node.val
-            path.append(node.val)
-
-            if not node.left and not node.right:
-                if current_sum == targetSum:
-                    total_paths.append(path[:])  # Make a copy of the path
-                    
-            else:
-                dfs(node.left, current_sum, path)
-                dfs(node.right, current_sum, path)
-
-            path.pop()  # Remove the last element from path before backtracking
+            current_path.append(node.val)
+            
+            remaining_sum -= node.val
+            
+            # Check if we are at a leaf node and if the remaining sum equals 0
+            if node.left is None and node.right is None and remaining_sum == 0:
+                # Add a copy of the current path to the result
+                result.append(list(current_path))  
+            
+            dfs(node.left, remaining_sum, current_path)
+            dfs(node.right, remaining_sum, current_path)
+            
+            # Backtrack by removing the last node from the path
+            current_path.pop()
         
-        dfs(root, 0, [])
-        return total_paths
+        dfs(root, targetSum, [])
+        
+        return result
+        
+        
