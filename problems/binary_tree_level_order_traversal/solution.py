9210440eb74632c23308129cfd281a848c8b44e0
Yashwanth RK <yashwanth1005@gmail.com>
2024-11-17 19:05:18 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (python3)

--- problems/binary_tree_level_order_traversal/solution.py
+++ problems/binary_tree_level_order_traversal/solution.py
@@ -9,26 +9,44 @@ class Solution:
         
         if not root:
             return []
-        
+
+        # bfs ==> queue
+
         from collections import deque
+
         queue = deque([root])
-        
-        result = []
+
+        levels = []
         
         while queue:
-            
-            level_items = []
-            
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level_items.append(node.val)
+            curr_level = []
 
+            size = len(queue)
+
+            for i in range(size):
+                node = queue.popleft()
+                curr_level.append(node.val)
                 if node.left:
                     queue.append(node.left)
-                
                 if node.right:
                     queue.append(node.right)
-                    
-            result.append(level_items)
 
-        return result
+
+            levels.append(curr_level)
+
+        return levels         
+
+
+        # while queue is not empty:
+        #     initialize curr_level as an empty list
+        #     determine size of current level as len(queue)
+
+        #     for i in range(size of current level):
+        #         pop a node from queue
+        #         add node value to curr_level
+        #         if left child exists, append to queue
+        #         if right child exists, append to queue
+
+        #     append curr_level to levels
+
+        # return levels   
