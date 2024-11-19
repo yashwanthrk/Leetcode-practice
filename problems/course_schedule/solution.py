c488035b489f12c0b1c579db89fdbdf156a2eb35
Yashwanth RK <yashwanth1005@gmail.com>
2024-11-19 05:57:23 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- problems/course_schedule/solution.py
+++ problems/course_schedule/solution.py
@@ -1,29 +1,36 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
+        # https://www.youtube.com/watch?v=EUDwWbvtB_Q&ab_channel=AlgoEngine
 
-        prereqs = defaultdict(list)
-        for c, p in prerequisites:
-            prereqs[c].append(p)
+       # Build adjacency list and calculate in-degrees
+        adj_list = defaultdict(list)
+        in_degree = [0] * numCourses
         
-        def cycle(course, seen):
-            # print(course, seen)
-            
-            if course in seen:
-                return True
-            seen.add(course)
-            for p in prereqs[course]:
-                if cycle(p, seen):
-                    return True
-            # print(prereqs, prereqs[course])
-            prereqs[course] = []
+        for course, prereq in prerequisites:
+            adj_list[prereq].append(course)  # prereq -> course
+            in_degree[course] += 1
 
-            seen.remove(course)
-            return False
-            
-        seen = set()
-            
-        for course in range(numCourses):
-            if cycle(course, seen):
-                return False
-        return True
+            # The adjacency list should represent the dependency direction, i.e., u -> v means you must complete v before u.
+        
+        # Initialize the queue with courses having no prerequisites
+        
+        queue = deque([])
+
+        for i in range(numCourses): 
+            if in_degree[i] == 0:
+                queue.append(i)
+        
+        visited = 0
+        
+        while queue:
+            node = queue.popleft()
+            visited += 1
+            for neighbor in adj_list[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        # If you can process all numCourses nodes, it means the graph is a DAG (no cycles), and a topological order exists.
+        # If all courses are visited, it means we can finish them
+        return visited == numCourses
