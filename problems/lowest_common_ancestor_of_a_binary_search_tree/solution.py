d76c4d400ff4b5baa713456804956c103b778e30
Yashwanth RK <yashwanth1005@gmail.com>
2024-11-21 17:13:48 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Search Tree (python3)

--- problems/lowest_common_ancestor_of_a_binary_search_tree/solution.py
+++ problems/lowest_common_ancestor_of_a_binary_search_tree/solution.py
@@ -7,14 +7,26 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while root:
-            # If both p and q are greater than root, move to the right subtree
-            if p.val > root.val and q.val > root.val:
-                root = root.right
-            # If both p and q are smaller than root, move to the left subtree
-            elif p.val < root.val and q.val < root.val:
-                root = root.left
-            else:
-                # If one node is smaller and the other is larger, or one of the nodes is root,
-                # then the current root is the LCA
-                return root
+        
+        def dfs(node, p, q):
+            
+            if not node:
+                return None
+
+            # If node is either p or q, return the node itself
+            if node == p or node == q:
+                return node
+
+            # If p and q are on different sides, the current node is the LCA
+            if (p.val < node.val < q.val) or (q.val < node.val < p.val):
+                return node
+
+            # Traverse the left subtree if both p and q are smaller than node
+            if p.val < node.val and q.val < node.val:
+                return dfs(node.left, p, q)
+
+            # Traverse the right subtree if both p and q are greater than node
+            if p.val > node.val and q.val > node.val:
+                return dfs(node.right, p, q)
+
+        return dfs(root, p, q)
