9a09ba16ef2681c22a6067b862759ede089bb0de
Yashwanth RK <yashwanth1005@gmail.com>
2024-11-22 12:00:48 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- problems/top_k_frequent_elements/solution.py
+++ problems/top_k_frequent_elements/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        frequencies = {}
-        
-        for element in nums:
-            frequencies[element] = frequencies.get(element, 0) + 1
-        
-        # Sort elements based on their frequencies in descending order
-        sorted_elements = sorted(frequencies, key=lambda x: frequencies[x], reverse=True)
-        
-        # Select the top k frequent elements
-        print(sorted_elements)
 
-        top_k_elements = sorted_elements[:k]
-        return top_k_elements
+        from collections import Counter
+        import heapq
+
+        num_freq = Counter(nums)
+        heap = []
+        for num, freq in num_freq.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (freq, num))
+            else:
+                heapq.heappushpop(heap, (freq, num))
+
+        return [num for freq, num in heap]
