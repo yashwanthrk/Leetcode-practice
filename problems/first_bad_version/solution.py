49f63aa4f3e575722a914ba52ef745e754b71040
Yashwanth RK <yashwanth1005@gmail.com>
2024-12-01 20:09:25 +0000

    Sync LeetCode submission - First Bad Version (python3)

--- problems/first_bad_version/solution.py
+++ problems/first_bad_version/solution.py
@@ -3,16 +3,19 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        i = 1
-        res = -1
-        while (i <= n):
-            mid = ( i + n ) // 2
-            if isBadVersion(mid) == False:
-                i = mid + 1
-            else:
-                res = mid
-                n = mid - 1
-        return res
+        
+        start = 1
+        result = -1
+        
+        while (start <= n):
             
+            mid = (start + n) // 2
 
-                
+            if isBadVersion(mid) == False:
+                start = mid + 1
+
+            else:
+                result = mid
+                n = mid - 1
+
+        return result
