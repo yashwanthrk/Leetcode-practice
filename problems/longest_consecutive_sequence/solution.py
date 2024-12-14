1141a65620e48a94497c8c597795c7bfb4f59198
Yashwanth RK <yashwanth1005@gmail.com>
2024-12-14 02:59:46 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (python3)

--- problems/longest_consecutive_sequence/solution.py
+++ problems/longest_consecutive_sequence/solution.py
@@ -1,24 +1,40 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
+        if not nums:
+            return 0
 
-        longest_streak = 0
-        num_set = set(nums)
-        
-        for num in num_set:
+        nums.sort()
+        longest_streak = 1
+        current_streak = 1
 
-            # Check if 'num' is the start of a sequence
-            if num - 1 not in num_set:
-                current_num = num
-                current_streak = 1
-                
-                # Count consecutive numbers
-                while current_num + 1 in num_set:
-                    current_num += 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:  # Skip duplicates
+                if nums[i] == nums[i - 1] + 1:
                     current_streak += 1
-                
-                longest_streak = max(longest_streak, current_streak)
-        
-        return longest_streak
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
 
-        # time and spac = O(n)
+        return max(longest_streak, current_streak)
+
+        # longest_streak = 0
+        # num_set = set(nums)
+        
+        # for num in num_set:
+
+        #     # Check if 'num' is the start of a sequence
+        #     if num - 1 not in num_set:
+        #         current_num = num
+        #         current_streak = 1
+                
+        #         # Count consecutive numbers
+        #         while current_num + 1 in num_set:
+        #             current_num += 1
+        #             current_streak += 1
+                
+        #         longest_streak = max(longest_streak, current_streak)
+        
+        # return longest_streak
+
+        # # time and spac = O(n)
