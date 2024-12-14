260e8920dc073f4120dcfef821e76750b27112db
Yashwanth RK <yashwanth1005@gmail.com>
2024-12-14 01:51:49 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- problems/merge_intervals/solution.py
+++ problems/merge_intervals/solution.py
@@ -1,22 +1,31 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
-        
-        # Step 1: Sort intervals by start time
+
+
         intervals.sort()
+
 
         merged_intervals = [intervals[0]]
 
-        for index in range(1, len(intervals)):
-            
-            prev_interval = merged_intervals[-1]
 
-            # If the current interval overlaps with the previous one
-            if intervals[index][0] <= prev_interval[1]:
-                new_start_time =  min(prev_interval[0], intervals[index][0])
-                new_end_time = max(prev_interval[1], intervals[index][1])
-                merged_intervals[-1] = [new_start_time, new_end_time]
+        for index in range(1, len(intervals)):
+
+        
+            old_interval = merged_intervals[-1]
+
+            if old_interval[1] >=  intervals[index][0]:
+
+                start_time = min(old_interval[0], intervals[index][0])
+                end_time = max(old_interval[1], intervals[index][1])
+                merged_intervals.pop()
+                merged_intervals.append((start_time, end_time))
+
             else:
+
                 merged_intervals.append(intervals[index])
 
+
         return merged_intervals
+                
+
+
